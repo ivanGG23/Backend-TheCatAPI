fed7519ca0ae9d2132ef26eb95ed3bffd24bba00
ivanGG23 <243836@ids.upchiapas.edu.mx>
2026-03-06 05:10:52 -0600

    cambio de ruta intento 2

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const errorHandler = require('./middlewares/errorHandler');
 const app = express();
 
 app.use(cors({
-    origin: ['http://localhost:8080', 'http://127.0.0.1:5500', 'https://frontendthecatapi.vercel.app/']
+    origin: ['http://localhost:8080', 'http://127.0.0.1:5500', 'https://frontendthecatapi.vercel.app/', 'frontendthecatapi-is0jqspnb-ivangg23s-projects.vercel.app', ]
 }));
 
 app.use(express.json());
